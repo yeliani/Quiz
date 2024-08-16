87f9a4cacb8935bcd003a661e64ffc79cea4a045
yeliani <158292677+yeliani@users.noreply.github.com>
2024-08-16 17:44:11 -0500

    Add files via upload

---
+++ algoritmos/index.js
@@ -0,0 +1,34 @@
+const Tarea =[
+    { id:1,
+    nombre:"correr",
+    completado:"falso"
+
+    },
+
+    { id:2,
+        nombre:"caminar",
+        completado:"falso"
+    
+        },
+        
+  { id:3,
+    nombre:"trotar",
+    completado:"completado"
+
+    },
+
+
+
+];
+
+const Tareacompleta = (array)=>{
+array.forEach(element); {
+    if((elemen.id==elemen.completado)&& elemen.id=="false")
+    
+    return Tareacompleta(elemen)
+};
+console.log (Tareacompleta(Tarea))
+
+
+
+}
